116d56707338ea5fc8f7b21844eede476e6e0f6a
calininja <syg0713@gmail.com>
2022-04-21 17:42:44 +0900

    fix: long schedule length bug

--- calendar.js
+++ calendar.js
@@ -9,6 +9,7 @@ let current = TODAY;
 
 // ### 달력 ###
 function factoryDate(realDate) {
+  // factoryDate 함수는 현재 날짜들을 미리 저장한 함수입니다.
   const year = realDate.getFullYear();
   const month = realDate.getMonth();
   const date = realDate.getDate();
@@ -24,6 +25,7 @@ function factoryDate(realDate) {
 }
 
 function makeMonthYearRow(realDate) {
+  // makeMonthYearRow 함수에서는 인자를 받아와서 날짜 데이터들을 돔 문자열로 만드는 함수입니다.
   const { currentYear, currentMonth } = factoryDate(realDate);
   let yearmonth = "";
   yearmonth = `<div class="year-month"><em>${
@@ -33,6 +35,8 @@ function makeMonthYearRow(realDate) {
 }
 
 function makeDatesRows(realDate) {
+  // makeDatesRows 함수에서는 인자를 받아와서 요일, 이전, 현재, 다음 날짜들을 각각 반복문을 돌려서 돔 문자열로 만들어
+  // 배열에 푸시 해주었고, result 객체로 반환 해주었습니다.
   const {
     currentYear,
     currentMonth,
@@ -99,6 +103,7 @@ function makeDatesRows(realDate) {
 }
 
 function renderCalendar(realDate) {
+  // renderCalendar 함수에서는 위의 날짜를 그리는 함수들에 current를 인자로 전달하여 날짜를 생성한 뒤 html에 그려주는 함수 입니다.
   const { yearmonth } = makeMonthYearRow(realDate);
   const { days, dates } = makeDatesRows(realDate);
 
@@ -108,9 +113,15 @@ function renderCalendar(realDate) {
 }
 
 function renderLabel() {
+  // renderLabel 함수는 라벨을 렌더링 해주는 함수입니다.
+  // 필요한 데이터들이 들어있는 스토리지 객체를 미리 만든 뒤 반복문을 돌려서
+  // 돔 데이터셋과 스토리지 데이터의 값이 일치하면 라벨로 렌더링을 해주었습니다.
+  // 하루 스케줄은 그대로 렌더링 해주었고, 긴 스케줄의 경우, 남은 요일의 일수 만큼 잘라 렌더링 해주고,
+  // 나머지 날짜는 다음 줄에 따로 렌더링 해주었습니다.
   if (SAVED_LABELS != null) {
     JSON.parse(SAVED_LABELS).forEach((storageItem) => {
       const STORAGE_OBJ = {
+        // 로컬스토리지와 관련된 관심사가 비슷한 데이터들을 객체형태로 묶음.
         month: new Date(storageItem.name).getMonth(),
         date: new Date(storageItem.name).getDate(),
         day: new Date(storageItem.name).getDay(),
@@ -149,8 +160,14 @@ function renderLabel() {
               SPAN.style.width = `calc(100% * ${STORAGE_OBJ.count}`;
               dom.appendChild(SPAN);
             }
-
-            makeFakeDom(index + 2, STORAGE_OBJ.count);
+            let tmp = "";
+            tmp = Number(new Date(dom.dataset.date));
+            if (tmp < Number(new Date(dom.dataset.date))) {
+              tmp = Number(new Date(dom.dataset.date));
+              makeFakeDom(index + 2, STORAGE_OBJ.count);
+            } else {
+              makeFakeDom(index + 3, STORAGE_OBJ.count);
+            }
           } else {
             // length 1(length 1)
             dom.appendChild(SPAN);
@@ -162,6 +179,9 @@ function renderLabel() {
 }
 
 function renderAdditionalLabel(storage_obj, splitted_count, rest_count) {
+  // renderAdditionalLabel 함수는 renderLabel에서 잘려진 나머지 일수들을 렌더링하는 함수입니다.
+  // 스토리지 객체, 잘려진 나머지 개수를 각각 받아서, 시작 날짜를 구한 뒤
+  // 반복문을 돌려서 시작 날짜와 일치하는 돔을 찾아서, 나머지일수 만큼의 너비로 렌더링 해주었습니다.
   const { month, date, content, color } = storage_obj;
   const { currentYear } = factoryDate(current);
   const REST_START_DATES = new Date(
@@ -183,21 +203,25 @@ function renderAdditionalLabel(storage_obj, splitted_count, rest_count) {
 }
 
 function makeFakeDom(index, count = 0) {
-  for (let i = 0; i < count; i++) {
+  // makeFakeDom 함수는 라벨의 가짜 돔을 만들어주는 함수 입니다.
+  // 긴 스케줄 데이터는 너비 값을 늘려서 구현을 해주었는데, 시작 날짜 컬럼 빼고는 형체가 없는 상태라
+  // 가짜 돔을 각각 심어주어서 높이 값을 맞춰 주었습니다.
+  // 긴 스케줄 데이터가 두번째 컬럼에 오는 케이스의 경우, 높이 값이 고정이라 라벨이 겹치는 버그가 발생하는데
+  // 플렉스 오더 속성으로 긴 스케줄이 먼저 정렬 되도록하여 해결 하였습니다.
+
+  for (let i = 0; i < count - 1; i++) {
     const date = document.querySelector(`.date:nth-of-type(${index + i - 1})`);
     const fake = document.createElement("div");
     if (date !== null) {
-      if (date.getAttribute("class").indexOf("hasExtra") != -1) {
-      } else {
-        fake.classList.add("fake");
-        date.appendChild(fake);
-      }
+      fake.classList.add("fake");
+      date.appendChild(fake);
     }
   }
 }
 
 document.querySelector(".clear").addEventListener("click", function () {
   localStorage.clear();
+  window.location.reload();
 });
 
 (function init() {
--- dialog.js
+++ dialog.js
@@ -9,11 +9,13 @@ let parsedLabels = JSON.parse(SAVED_LABELS) || [];
 let eventTitle = "";
 
 function saveLabel() {
+  // saveLabels 함수는 로컬스토리지에 저장하는 함수입니다.
   localStorage.setItem("calendar", JSON.stringify(parsedLabels));
 }
 
 DATES.addEventListener("click", injectDialog);
 function injectDialog(e) {
+  // injectDialog 함수는 이벤트 타겟으로 데이터셋을 받아서 타이틀을 넣어 그려주는 함수입니다.
   const { date: TITLE } = e.target.dataset;
   document.querySelector(".overlay").classList.add("active");
   document.querySelector("h3").innerHTML = `${TITLE.split(" ")[0]}, ${
@@ -24,6 +26,8 @@ function injectDialog(e) {
 
 SUBMIT.addEventListener("click", onSubmit);
 function onSubmit(e) {
+  // 서브밋 함수는 이벤트 타이틀 값과 인풋, 피커, 카운터 값을 받아서
+  // 캘린더에 선언 되어있는 로컬스토리지에 객체 형태로 푸시를 해주는 함수입니다.
   e.preventDefault();
   if (!INPUT.value) return alert("내용을 입력 해주세요.");
   const OBJ_CONTENT = {
@@ -40,5 +44,6 @@ function onSubmit(e) {
 
 CLOSE.addEventListener("click", onclose);
 function onclose() {
+  // 다이얼로그를 닫는 함수 입니다. 돔은 css와 클래스로 조작 하였습니다.
   document.querySelector(".overlay").classList.remove("active");
 }
